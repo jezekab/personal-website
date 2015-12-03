a6579aa87cf0367c8b629f7117d0801af779f002
jezekab <jezekab@gmail.com>
2015-12-04 09:50:54 +1000

    cleaned up file structure

---
+++ archive/v2/script.js
@@ -0,0 +1,27 @@
+//to browser size on load
+$(function() {
+  var h = window.innerHeight;
+  $(".toWindowSize").css("height", h);
+});
+
+//to browser size on resize
+$(window).resize(function() {
+  var h = window.innerHeight;
+  $(".toWindowSize").css("height", h);
+});
+
+//smooth scroll
+$(".scroll").click(function(event){
+  event.preventDefault();
+  $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
+//toggle responsive menu
+$(function() {
+  //hides menu initially
+  $('.mobile_links').hide();
+  $("#menu").click(function() {
+    //show/hide menu on click
+    $( ".mobile_links" ).slideToggle( "slow" );
+  });
+});
